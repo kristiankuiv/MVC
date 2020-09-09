cb46f1c8eb326e25e88bfea2ab53e68501a613ba
krsitiankuiv <Muri54321>
2020-09-09 09:02:43 +0300

    loodud abifunktsionaalsus

--- helpers.py
+++ helpers.py
@@ -2,8 +2,23 @@ elemendid = []
 
 # lisame ELEMENT juurde
 def lisa_element(nimetus, hind, kogus):
+    global elemendid
     elemendid.append({"nimetus":nimetus, "hind":hind,"kogus":kogus})
 
 #lisame ELEMENDID KORRAGA juurde
 def lisa_elemendid(elementide_nimekiri):
+    global elemendid
     elemendid = elementide_nimekiri
+
+#loome katseandmestik
+katse_elemendid = [
+    {"nimetus": "leib", "hind":0.80, "kogus": 20},
+    {"nimetus": "piim", "hind":0.50, "kogus": 15},
+    {"nimetus": "vein", "hind":5.60, "kogus": 5},
+]
+#testime elementide lisamist
+lisa_elemendid(katse_elemendid)
+print(elemendid)
+
+#testime üksiku elemendi lisamist
+lisa_element("kohupiim", 0.90, 15)
